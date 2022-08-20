6d22df3bdb9f6b38f54578633df8e2995e7ccad0
Seonu <seonup95@gmail.com>
2022-08-20 17:44:54 +0900

    Control movie's summary length

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -8,14 +8,14 @@ const Movie = ({ id, coverImg, title, summary, genres }) => {
       <h2>
         <Link to={`/movie/${id}`}>{title}</Link>
       </h2>
-      <p>{summary}</p>
+      <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
       <ul>
         {genres.map((g) => (
           <li key={g}>{g}</li>
         ))}
       </ul>
     </div>
-  );
+  );å
 };
 
 Movie.propTypes = {
